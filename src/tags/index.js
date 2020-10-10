935c38caf0bd1cacdb43bd02c412b2b811a44daa
CloverHearts <cloverheartsdev@gmail.com>
2020-10-11 02:39:10 +0900

    + remove debug console and added markdown feature for strikethrough

--- src/tags/index.js
+++ src/tags/index.js
@@ -4,6 +4,7 @@ import Bold from './bold'
 import InlineCode from './inlinecode'
 import Link from './link'
 import Codeblock from './codeblock'
+import Strikethrough from './strikethrough'
 
 class TagsOperators {
   constructor (quillJS) {
@@ -15,7 +16,8 @@ class TagsOperators {
       new Bold(this.quillJS).getAction(),
       new Link(this.quillJS).getAction(),
       new Codeblock(this.quillJS).getAction(),
-      new InlineCode(this.quillJS).getAction()
+      new InlineCode(this.quillJS).getAction(),
+      new Strikethrough(this.quillJS).getAction()
     ]
   }
 
