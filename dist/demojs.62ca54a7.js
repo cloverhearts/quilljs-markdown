c9fb019e67b57b7cb8e4fe921bde603f094443c1
CloverHearts <cloverheartsdev@gmail.com>
2021-04-27 00:01:15 +0900

    ISSUE-36 Improve css

--- dist/demojs.62ca54a7.js
+++ dist/demojs.62ca54a7.js
@@ -20799,7 +20799,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62146" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51480" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
